1ab02199c2810ab261efd0451d9e9b9349013746
Sodiq <sodiq.onawale04@gmail.com>
2023-05-13 22:16:51 -0400

    let todo app use window local storage to save user todo items

--- src/Components/TodoApp/TodoApp.js
+++ src/Components/TodoApp/TodoApp.js
@@ -7,52 +7,78 @@ import TodoFooter from "../TodoFooter/TodoFooter";
 let nextId = 0;
 
 function TodoApp() {
-        const [todoItems, setTodoItems] = useState([]);
+    const [todoItems, setTodoItems] = useState(
+        JSON.parse(localStorage.getItem('items')) || []
+    );
 
-        const [isChecked, setIsChecked] = useState(null)
+    const [isChecked, setIsChecked] = useState(
+        typeof JSON.parse(localStorage.getItem('isChecked')) === 'boolean' ? JSON.parse(localStorage.getItem('isChecked')) : null
+    );
 
-        function handleTodoItems(inputValue) {
-            if (inputValue) {
-                setTodoItems([
+    function handleTodoItems(inputValue) {
+        if (inputValue) {
+            setTodoItems([
+                ...todoItems,
+                {
+                    inputValue: inputValue,
+                    checked: false,
+                    id: nextId++,
+                },
+            ]);
+            console.log(todoItems)
+            localStorage.setItem('items', JSON.stringify(
+                [
                     ...todoItems,
                     {
                         inputValue: inputValue,
                         checked: false,
                         id: nextId++,
                     },
-                ]);
-                }
+                ]
+            ));      
         }
+    }
 
     function handleRemoveTodo(todoItem) {
-        const todo = todoItems.filter(
-        (currentTodoItem) => currentTodoItem.id !== todoItem.id
-        );
+        const todo = todoItems.filter(currentTodoItem => currentTodoItem.id !== todoItem.id);
         setTodoItems(todo);
+        localStorage.setItem('items', JSON.stringify(todo));      
     }
 
     function handleChecked(itemID, nextCheck) {
         setTodoItems(
-        todoItems.map((todoItem) => {
-            if (todoItem.id === itemID) {
-            return { ...todoItem, checked: nextCheck };
-            } else {
-            return todoItem;
-            }
-        })
+            todoItems.map((todoItem) => {
+                if (todoItem.id === itemID) {
+                return { ...todoItem, checked: nextCheck };
+                } else {
+                return todoItem;
+                }
+            })
         );
+        localStorage.setItem('items', JSON.stringify(
+            todoItems.map((todoItem) => {
+                if (todoItem.id === itemID) {
+                return { ...todoItem, checked: nextCheck };
+                } else {
+                return todoItem;
+                }
+            })
+        ));      
     }
 
     function handleAllTodo() {
         setIsChecked(null)
+        localStorage.setItem('isChecked', JSON.stringify(null))
     }
 
     function handleActiveTodo() {
         setIsChecked(false)
+        localStorage.setItem('isChecked', JSON.stringify(false))
     }
 
     function handleCompletedTodo() {
         setIsChecked(true)
+        localStorage.setItem('isChecked', JSON.stringify(true))
     }
 
     const visibleTodos = todoItems.filter(item => {
@@ -65,6 +91,7 @@ function TodoApp() {
     function handleClearCompletedTodo() {
         const  activeTodos = todoItems.filter(item =>  item.checked === false)
         setTodoItems(activeTodos)
+        localStorage.setItem('items', JSON.stringify(activeTodos));      
     }
 
     return (
